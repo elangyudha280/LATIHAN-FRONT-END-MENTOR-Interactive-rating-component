f2ea1478f2b6bc72be5d8bca55b0721567474750
elangyudha280 <elangyudharakasiwi@gmail.com>
2022-07-03 20:15:29 +0700

    fix bug

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 
 let card = document.querySelector('.card-page')
+let card_thanks = document.querySelector('.card-thanks')
 let number_item = document.querySelectorAll('.number-item')
 card.addEventListener('click',function(el){
 
@@ -14,4 +15,17 @@ card.addEventListener('click',function(el){
         })
         el.target.classList.add('active-number')
     }
+})
+
+let button_submit = document.querySelector('.btn-submit')
+let num_selected = document.querySelector('.num-selected')
+button_submit.addEventListener('click',function(e){
+    card.style.display = 'none';
+    card_thanks.style.display ='initial'
+    number_item.forEach((e) => {
+        if(e.classList.contains('active-number') ){
+            num_selected.textContent = e.textContent;
+        }
+     
+    })
 })
